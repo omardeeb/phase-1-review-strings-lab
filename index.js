f524b17fd754fe9124badb8a0625067143a60470
oamr <or.deeb@gmail.com>
2022-10-09 21:19:20 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'string';
+let welcomeMessage = 'Welcome to Flatbook, ';
+ welcomeMessage = currentUser + 'Welcome to Flatbook, !';
+ let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+ excitedWelcomeMessage = currentUser.toLocaleUpperCase() + 'WELCOME TO FLATBOOK, !';
+ let shortGreeting = 'Welcome, ';
+ const firstInitial = currentUser[0]
+ shortGreeting = 'Welcome, ' + firstInitial + '!';
